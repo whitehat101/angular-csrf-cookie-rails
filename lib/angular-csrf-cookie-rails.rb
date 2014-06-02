cea9cdb85a3fbab08d71ea66aa04606b4499e707
Jeremy Ebler <jebler@gmail.com>
2014-06-01 17:27:49 -0700

    ::

--- lib/angular-csrf-cookie-rails.rb
+++ lib/angular-csrf-cookie-rails.rb
@@ -6,8 +6,8 @@ module AngularCSRFCookieRails
     # config.after_initialize do
     initializer "angular-csrf-cookie-rails" do |app|
       require 'action_controller/base'
-      ActionController::Base.send :include, AngularCSRFCookieRails::ActionController
-      ActionController::Base.send :after_filter, :set_csrf_cookie_for_ng
+      ::ActionController::Base.send :include, AngularCSRFCookieRails::ActionController
+      ::ActionController::Base.send :after_filter, :set_csrf_cookie_for_ng
     end
   end
 end
