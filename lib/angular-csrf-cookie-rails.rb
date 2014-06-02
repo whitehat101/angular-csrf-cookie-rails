203a36f3fb2bc5b6a4ea7a76176efaa05a10d070
Jeremy Ebler <jebler@gmail.com>
2014-06-01 17:17:10 -0700

    send :after_filter to ActionController::Base

--- lib/angular-csrf-cookie-rails.rb
+++ lib/angular-csrf-cookie-rails.rb
@@ -5,6 +5,7 @@ module AngularCSRFCookieRails
   class Engine < ::Rails::Engine
     initializer "angular-csrf-cookie-rails" do |app|
       ActionController::Base.send :include, AngularCSRFCookieRails::ActionController
+      ActionController::Base.send :after_filter, :set_csrf_cookie_for_ng
     end
   end
 end
--- lib/angular-csrf-cookie-rails/action_controller.rb
+++ lib/angular-csrf-cookie-rails/action_controller.rb
@@ -1,7 +1,5 @@
 module AngularCSRFCookieRails
   class ActionController
-    after_filter :set_csrf_cookie_for_ng
-
     def set_csrf_cookie_for_ng
       cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
     end
