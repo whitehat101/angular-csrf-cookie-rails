50eda36133a7cc9f44d1a13fc7d2be9f5b8b29b4
Jeremy Ebler <jebler@gmail.com>
2014-06-01 17:26:30 -0700

    action-pack, require action-controller

--- lib/angular-csrf-cookie-rails.rb
+++ lib/angular-csrf-cookie-rails.rb
@@ -3,7 +3,9 @@ require "angular-csrf-cookie-rails/action_controller"
 
 module AngularCSRFCookieRails
   class Engine < ::Rails::Engine
-    config.after_initialize do
+    # config.after_initialize do
+    initializer "angular-csrf-cookie-rails" do |app|
+      require 'action_controller/base'
       ActionController::Base.send :include, AngularCSRFCookieRails::ActionController
       ActionController::Base.send :after_filter, :set_csrf_cookie_for_ng
     end
