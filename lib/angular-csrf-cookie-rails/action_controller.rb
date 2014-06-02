464cd367c8fc44bbca14802c4c99d28503fe555d
Jeremy Ebler <jebler@gmail.com>
2014-06-01 17:31:44 -0700

    included

--- lib/angular-csrf-cookie-rails/action_controller.rb
+++ lib/angular-csrf-cookie-rails/action_controller.rb
@@ -1,6 +1,9 @@
 module AngularCSRFCookieRails
   module ActionController
-    after_filter :set_csrf_cookie_for_ng
+    def self.included klass
+      klass.after_filter :set_csrf_cookie_for_ng
+    end
+
     def set_csrf_cookie_for_ng
       cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
     end
